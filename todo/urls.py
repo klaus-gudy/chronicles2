3232a3ab5418cc6317187abeaca1cc456d2fc2b3
klaus-gudy <84222795+klaus-gudy@users.noreply.github.com>
2021-08-08 10:23:49 +0300

    messages

--- todo/urls.py
+++ todo/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('create/', views.create, name='create'),
     path('details/<id>/', views.details, name='details'),
     path('delete/<id>/', views.delete, name='delete'),
+    path('edit/<id>/', views.edit, name='edit'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 from django.shortcuts import get_object_or_404
+from django.contrib  import messages
 
 def index(request):
     todos = Todo.objects.all()
@@ -36,6 +37,8 @@ def create(request):
 
         todo.save()
 
+        messages.add_message(request, messages.SUCCESS, "CREATED successful")
+
         return HttpResponseRedirect(reverse("details", kwargs={'id': todo.pk}))
 
     return render(request,  'todo/create.html', context)    
@@ -54,6 +57,32 @@ def delete(request, id):
 
     if request.method == "POST":
         todo.delete()
+
+        messages.add_message(request, messages.SUCCESS, "deleted successful")
+
         return HttpResponseRedirect(reverse('index'))
 
     return render(request,  'todo/delete.html', context )      
+
+def edit(request, id):
+    todo = get_object_or_404(Todo, pk = id)
+    form= TodoForm(instance=todo)
+
+    context = {'todo': todo , 'form':form}
+
+    if request.method == "POST":
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        is_completed = request.POST.get('is_completed', False)
+
+        todo.title = title
+        todo.description = description
+        todo.is_completed = True if is_completed =="on" else False
+
+        todo.save()
+
+        messages.add_message(request, messages.SUCCESS, "edit successful")
+
+        return HttpResponseRedirect(reverse("details", kwargs={'id': todo.pk}))
+
+    return render(request, 'todo/edit.html', context)    
